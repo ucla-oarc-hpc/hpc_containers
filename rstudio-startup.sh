4d2c9647dbed60679024b6090752abb4b1b3cfc8
charliecpeterson <charliecpeterson@gmail.com>
2022-08-26 13:48:10 -0700

    Rstudio updates

--- rstudio-startup.sh
+++ rstudio-startup.sh
@@ -13,10 +13,10 @@ export HOST=`hostname`
 
 export USER=`whoami`
 
-echo  "\n\nThis is the Rstudio server container running R 4.1.0 from Rocker"
+echo  "\n\nThis is the Rstudio server container running R 4.2.1 from Rocker"
 echo ""
 echo "This is a separate R version from the rest of Hoffman2"
-echo "When you install libraries from this Rstudio/R, they will be in ~/R/APPTAINER/h2-rstudio_4.1.0"
+echo "When you install libraries from this Rstudio/R, they will be in ~/R/APPTAINER/h2-rstudio_4.2.1"
 echo ""
 echo  "Your Rstudio server is running on: ${GREEN} ${HOST} ${NOCOLOR}"
 echo  "It is running on PORT: ${GREEN} ${PORT} ${NOCOLOR}"
@@ -28,4 +28,4 @@ echo  "Then open your web browser to ${GREEN} http://localhost:${PORT} ${NOCOLOR
 echo ""
 echo "Please run [CTRL-C] on this process to exit Rstudio"
 echo ""
-rserver --www-port=${PORT}
+rserver --server-user=${USER} --www-port=${PORT}
