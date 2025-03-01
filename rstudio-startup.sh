95d7bfda90127e4f9e5ea634fd7e0c4e3daf467e
charliecpeterson <charliecpeterson@gmail.com>
2025-03-01 09:59:20 -0800

    updates

--- rstudio-startup.sh
+++ rstudio-startup.sh
@@ -1,34 +1,75 @@
+#!/bin/bash
+
 rserverport="${R_SERVER_PORT:-8787}"
-GREEN='\033[0;32m'
-NOCOLOR='\033[0m'
-checkport=`ss -tulpn | grep ":${rserverport}"`
-while [ ! -z "$checkport" ]
-do
-   rserverport=`echo ${rserverport} + 1 | bc`
-   echo $rserverport
-   checkport=`ss -tulpn | grep ":${rserverport}"`
+if [[ -t 1 ]]; then
+  GREEN='\033[0;32m'
+  NOCOLOR='\033[0m'
+else
+  GREEN=''
+  NOCOLOR=''
+fi
+
+# Create R lib directory
+if [ ! -d "$HOME/R/APPTAINER/h2-rstudio_${R_VER}" ]; then
+    mkdir -p "$HOME/R/APPTAINER/h2-rstudio_${R_VER}"
+fi
+
+# Check if there are any arguments
+echo "test $*"
+echo "test2 $@"
+echo "test3 $1"
+echo "test4 $#"
+echo "Number of args: $#"
+echo "Args as string: $*"
+echo "Args individually:"
+for arg in "$@"; do
+  echo "$arg"
 done
-export PORT=$rserverport
-export HOST=`hostname`
 
-export USER=`whoami`
-export PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)
+if [ -z "$*" ]; then
 
-echo  "\n\nThis is the Rstudio server container running R ${R_VER} from Rocker"
-echo ""
-echo "This is a separate R version from the rest of Hoffman2"
-echo "When you install libraries from this Rstudio/R, they will be in ~/R/APPTAINER/h2-rstudio_${R_VER}"
-echo ""
-echo  -e "Your Rstudio server is running on: ${GREEN} ${HOST} ${NOCOLOR}"
-echo  -e "It is running on PORT: ${GREEN} ${PORT} ${NOCOLOR}"
-echo ""
-echo "Open a SSH tunnel on your local computer by running:"
-echo  -e "${GREEN} ssh -N -L ${PORT}:${HOST}:${PORT} ${USER}@hoffman2.idre.ucla.edu ${NOCOLOR}"
-echo ""
-echo  -e "Then open your web browser to ${GREEN} http://localhost:${PORT} ${NOCOLOR}"
-echo ""
-echo -e "Your Rstudio USERNAME is: ${GREEN} ${USER} ${NOCOLOR}"
-echo -e "Your Rstudio PASSWORD is: ${GREEN} ${PASSWORD} ${NOCOLOR}"
-echo "Please run [CTRL-C] on this process to exit Rstudio"
-echo ""
-rserver --server-user=${USER} --www-port=${PORT} --auth-none=0 --auth-pam-helper-path=pam-helper
+  checkport=$(ss -tulpn | grep ":${rserverport}")
+  while [ -n "$checkport" ]; do
+    rserverport=$((rserverport + 1))
+    checkport=$(ss -tulpn | grep ":${rserverport}")
+  done
+
+  export PORT=$rserverport
+  export HOST="$(hostname)"
+  export USER="$(whoami)"
+  export PASSWORD="$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 10 | head -n 1)"    
+
+  echo
+  echo "This is the Rstudio server container running R ${R_VER} from Rocker"
+  echo
+  echo "This is a separate R version from the rest of Hoffman2"
+  echo "When you install libraries from this Rstudio/R, they will be in ~/R/APPTAINER/h2-rstudio_${R_VER}"
+  echo
+
+  # Use printf for colorized lines:
+  printf "Your Rstudio server is running on: ${GREEN}%s${NOCOLOR}\n" "${HOST}"
+  printf "It is running on PORT: ${GREEN}%s${NOCOLOR}\n" "${PORT}"
+  echo
+
+  echo "Open a SSH tunnel on your local computer by running:"
+  printf "${GREEN}ssh -N -L %s:%s:%s %s@hoffman2.idre.ucla.edu${NOCOLOR}\n" \
+         "${PORT}" "${HOST}" "${PORT}" "${USER}"
+  echo
+
+  printf "Then open your web browser to ${GREEN}http://localhost:%s${NOCOLOR}\n" "${PORT}"
+  echo
+
+  printf "Your Rstudio USERNAME is: ${GREEN}%s${NOCOLOR}\n" "${USER}"
+  printf "Your Rstudio PASSWORD is: ${GREEN}%s${NOCOLOR}\n" "${PASSWORD}"
+  echo "Please run [CTRL-C] on this process to exit Rstudio"
+  echo
+
+  rserver \
+    --server-user="${USER}" \
+    --www-port="${PORT}" \
+    --auth-none=0 \
+    --auth-pam-helper-path=pam-helper
+else
+  export R_LIBS_USER="$HOME/R/APPTAINER/h2-rstudio_${R_VER}"
+  exec "$@"
+fi
